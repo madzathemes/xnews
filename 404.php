d2a41381af1ce450a3b1c4f4389d7bfbf46720b9
madzathemes <madza@madzathemes.com>
2017-08-25 15:48:53 +0300

    fix

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 			<div class="heading-404">
 				<h2><?php esc_html_e( '404', 'xnews' ); ?></h2>
 				<h3><?php echo esc_html($t_p_nothing_found); ?></h3>
-				<p><?php esc_html_e( 'The requested term was not found on this server. That’s all we know.', 'xnews' ); ?></p>
+				<p><?php esc_html_e( 'The requested term was not found on this server. That is all we know.', 'xnews' ); ?></p>
 			</div>
 	</div>
 </div>
