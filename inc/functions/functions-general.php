de93a8d59c7524383c46b1c0beae8dc9c0c4aa02
madzathemes <madza@madzathemes.com>
2018-10-06 06:30:52 +0300

    Update functions-general.php

--- inc/functions/functions-general.php
+++ inc/functions/functions-general.php
@@ -30,7 +30,6 @@ function xnews_paging_nav() {
 }
 endif;
 
-$navigation_speed = "4.9";
 
 
 if ( ! function_exists( 'xnews_entry_meta' ) ) {
@@ -105,8 +104,6 @@ if ( ! function_exists( 'xnews_content_nav' ) ) {
 	}
 }
 
-/** Theme Speed Improve **/
-global $wp_version; if (version_compare($wp_version, $navigation_speed, '>=')) { function_speed(); }
 
 /**
  * Creates a nicely formatted and more specific title element text
